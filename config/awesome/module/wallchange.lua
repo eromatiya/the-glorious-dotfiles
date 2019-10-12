5908fd5c13acedc7cd1f7babdb2e79e442e625e4
Gerome Matilla <gerome@root.localdomain>
2019-10-12 21:01:40 +0800

    Update

--- config/awesome/module/wallchange.lua
+++ config/awesome/module/wallchange.lua
@@ -1,12 +1,23 @@
+-- This module changes wallpaper based on declared time
+-- Note that this module will not change the wallpaper accurately
+-- Because it only checks the time every 10 minutes/600 seconds
+-- So the wallpaper will change only within the first 10 mins of the hour
+
 local filesystem = require('gears.filesystem')
 local wall_dir = filesystem.get_configuration_dir() .. '/theme/wallpapers/'
 local gears = require('gears')
 local awful = require('awful')
 
+
+-- Change hour from 00-23
+local hour = 19
+local checkTimePer = 600 -- seconds
+
 function timeChecker()
   time = os.date("*t")
     time = time.hour
-    if tonumber(time) >= 19 then
+    -- Change to night-wallpaper when it's 7PM.
+        if tonumber(time) >= hour then
       gears.wallpaper.maximized (wall_dir .. 'night-wallpaper.jpg', s)
     else
       gears.wallpaper.maximized (wall_dir .. 'day-wallpaper.jpg', s)
@@ -15,7 +26,7 @@ end
 
 -- Check time every 10 mins
 local runCheck = gears.timer {
-  timeout   = 600,
+  timeout   = checkTimePer,
   autostart = true,
   call_now = true,
   callback  = function()
@@ -23,7 +34,7 @@ local runCheck = gears.timer {
   end
 }
 
--- Run Once When Invoked
+-- Run Once On Start-up
 local runOnce = gears.timer {
   timeout   = 0,
   call_now  = true,
