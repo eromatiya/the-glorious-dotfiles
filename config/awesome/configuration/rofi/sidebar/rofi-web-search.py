3e358c2c1ecb8ad22e514a1fc25b561bb279250c
Gerome Matilla <gerome@root.localdomain>
2019-10-15 18:59:46 +0800

    web search update

--- config/awesome/configuration/rofi/sidebar/rofi-web-search.py
+++ config/awesome/configuration/rofi/sidebar/rofi-web-search.py
@@ -1,8 +1,5 @@
 #!/bin/env python3
 
-
-# This script is made by pdonadeo
-# For more info: https://github.com/pdonadeo/rofi-web-search
 import json
 import re
 import urllib.parse
@@ -10,6 +7,7 @@ import urllib.request
 import sys
 import os
 import datetime
+import gzip
 
 import subprocess as sp
 
@@ -19,18 +17,25 @@ import html
 ################################################################################
 #####                      C O N F I G U R A T I O N                      ######
 ################################################################################
-SEARCH_ENGINE = 'google'    # or 'duckduckgo'
-BROWSER = 'firefox'          # or 'firefox'
+SEARCH_ENGINE = 'google'            # or 'duckduckgo'
+BROWSER = 'chrome'                  # or 'firefox', 'chromium', 'brave', 'lynx'
+TERMINAL = ['gnome-terminal', '--'] # or ['st', '-e'] or something like that
 ################################################################################
 
 CONFIG = {
     'BROWSER_PATH' : {
-        'chrome' : '/usr/bin/google-chrome-stable',
-        'firefox' : '/usr/bin/firefox'
+        'chrome' : ['google-chrome-stable'],
+        'firefox' : ['firefox'],
+        'chromium' : ['chromium-browser'],
+        'brave' : ['brave-browser'],
+        'lynx' : TERMINAL + ['lynx']
     },
     'USER_AGENT' : {
         'chrome' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
-        'firefox' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
+        'firefox' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+        'chromium' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/76.0.3809.100 Chrome/76.0.3809.100 Safari/537.36',
+        'brave' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+        'lynx' : 'Lynx/2.8.9rel.1 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/1.1.1d'
     },
     'SEARCH_ENGINE_NAME' : {
         'google' : 'Google',
@@ -64,7 +69,7 @@ def fetch_suggestions(search_string):
         headers = {
             'sec-fetch-mode' : 'cors',
             'dnt' : '1',
-            'accept-encoding' : 'gzip, deflate, br',
+            'accept-encoding' : 'gzip',
             'accept-language' : 'en-US;q=0.9,en;q=0.8',
             'pragma' : 'no-cache',
             'user-agent' : CONFIG['USER_AGENT'][BROWSER],
@@ -74,12 +79,17 @@ def fetch_suggestions(search_string):
             'referer' : 'https://www.google.com/',
             'sec-fetch-site' : 'same-origin'
         }
-        req = urllib.request.Request(url, headers={}, method='GET')
+        req = urllib.request.Request(url, headers=headers, method='GET')
 
-        reply_data = urllib.request.urlopen(req).read().split(b'\n')[1]
+        reply_data = gzip.decompress(urllib.request.urlopen(req).read()).split(b'\n')[1]
         reply_data = json.loads(reply_data)
         return [ cleanhtml(res[0]).strip() for res in reply_data[0] ]
     else:   # 'duckduckgo'
+        if search_string.startswith('!'):
+            bang_search = True
+            search_string = search_string.lstrip('!')
+        else:
+            bang_search = False
         r = {
             'q' : search_string,
             'callback' : 'autocompleteCallback',
@@ -87,21 +97,23 @@ def fetch_suggestions(search_string):
             '_' : str(int((datetime.datetime.now().timestamp())*1000))
         }
         url = CONFIG['SUGGESTION_URL'][SEARCH_ENGINE] + urllib.parse.urlencode(r)
+        if bang_search:
+            url = url.replace('?q=', '?q=!')
         headers = {
             'pragma' : 'no-cache',
             'dnt' : '1',
-            'accept-encoding' : 'gzip, deflate, br',
+            'accept-encoding' : 'gzip',
             'accept-language' : 'en-US;q=0.9,en;q=0.8',
             'user-agent' : CONFIG['USER_AGENT'][BROWSER],
             'sec-fetch-mode' : 'no-cors',
             'accept' : '*/*',
             'cache-control' : 'no-cache',
-            'authority' : 'duckduckgo.com',
+           'authority' : 'duckduckgo.com',
             'referer' : 'https://duckduckgo.com/',
             'sec-fetch-site' : 'same-origin',
         }
-        req = urllib.request.Request(url, headers={}, method='GET')
-        reply_data = urllib.request.urlopen(req).read().decode('utf8')
+        req = urllib.request.Request(url, headers=headers, method='GET')
+        reply_data = gzip.decompress(urllib.request.urlopen(req).read()).decode('utf8')
         reply_data = json.loads(re.match(r'autocompleteCallback\((.*)\);', reply_data).group(1))
         return [ cleanhtml(res['phrase']).strip() for res in reply_data ]
 
@@ -109,22 +121,20 @@ def main():
     search_string = html.unescape((' '.join(sys.argv[1:])).strip())
 
     if search_string.endswith('!'):
-        search_string = search_string.strip('!').strip()
+        search_string = search_string.rstrip('!').strip()
         results = fetch_suggestions(search_string)
         for r in results:
             print(html.unescape(r))
     elif search_string == '':
-        print('Search the web with %s' % CONFIG['SEARCH_ENGINE_NAME'][SEARCH_ENGINE])
-        print('Close search string with "!" to get suggestions')
-        print('Search engine can be change in the config')
-        print('This script is made by pdonadeo')
-        print('https://github.com/pdonadeo/rofi-web-search')
+        print('!!-- Type something and search it with %s' % CONFIG['SEARCH_ENGINE_NAME'][SEARCH_ENGINE])
+        print('!!-- Close your search string with "!" to get search suggestions')
     else:
         url = CONFIG['SEARCH_URL'][SEARCH_ENGINE] + urllib.parse.quote_plus(search_string)
-        sp.Popen([CONFIG['BROWSER_PATH'][BROWSER], url], stdout=sp.DEVNULL, stderr=sp.DEVNULL, shell=False)
+        sp.Popen(CONFIG['BROWSER_PATH'][BROWSER] + [url], stdout=sp.DEVNULL, stderr=sp.DEVNULL, shell=False)
 
 if __name__ == "__main__":
     try:
         main()
     except:
         sys.exit(1)
+ 
