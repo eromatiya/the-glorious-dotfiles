4ef84e660bd80f8cb9958e18e6e6d17a2fb84f78
XeGrox <xegrox@protonmail.com>
2019-12-08 15:48:56 +0800

    Fix some titlebar errors

--- config/awesome/rounded/module/titlebar-decorate-client.lua
+++ config/awesome/rounded/module/titlebar-decorate-client.lua
@@ -17,33 +17,38 @@ local roundCorners = function(cr, width, height)
   gears.shape.rounded_rect(cr, width, height, beautiful.corner_radius)
 end
 
+-- On Spawn
+_G.client.connect_signal("manage", function(c)
+  if not c.max then
+    awful.titlebar.show(c, 'left')
+  else
+    awful.titlebar.hide(c, 'left')
+  end
+end)
+
 _G.screen.connect_signal("arrange", function(s)
   for _, c in pairs(s.clients) do
-    if (#s.tiled_clients > 0 or c.floating) and (c.first_tag.layout.name ~= 'max' or not c.max) then
+    if (#s.tiled_clients > 1 or c.floating) and c.first_tag.layout.name ~= 'max' then
       if not c.hide_titlebars then
 	    awful.titlebar.show(c, 'left')
 	  else 
 		awful.titlebar.hide(c, 'left')
 	  end
+	  if c.floating and not c.skip_center then
+		awful.placement.centered(c)
+	  end
       if c.maximized then
         c.shape = function(cr, w, h)
-        gears.shape.rectangle(cr, w, h)
+          gears.shape.rectangle(cr, w, h)
+        end
+      else 
+        c.shape = roundCorners
       end
-    else 
-      c.shape = roundCorners
-    end
-
-    if c.floating and not c.skip_center then
-      awful.placement.centered(c)
-    end
-
-    else if c.first_tag.layout.name == 'max' or c.max then
+    elseif #s.tiled_clients == 1 or c.first_tag.layout.name == 'max' then
       awful.titlebar.hide(c, 'left')
       c.shape = function(cr, w, h)
-      gears.shape.rectangle(cr, w, h)
-    end
+        gears.shape.rectangle(cr, w, h)
       end
     end
-
   end
 end)
