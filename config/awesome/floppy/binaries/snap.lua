23948deead1551ed0a3b79edad149bf63c3db371
Gerome Matilla <gerome.matilla07@gmail.com>
2019-12-17 21:43:56 +0800

    update snap.lua binary

--- config/awesome/floppy/binaries/snap.lua
+++ config/awesome/floppy/binaries/snap.lua
@@ -25,7 +25,7 @@ local capture_screen = function(mode)
       notify-send 'Snap!' 'Area screenshot saved and copied to clipboard!'
       ;;
       'select')
-      maim -s -m 1 $dir/$(date +%Y%m%d_%H%M%S).png
+      maim -u -s -m 1 $dir/$(date +%Y%m%d_%H%M%S).png
       xclip -selection clipboard -t image/png -i $dir/`ls -1 -t $dir | head -1` &
       notify-send 'Snap!' 'Selected shot saved and copied to clipboard!'
       ;;
