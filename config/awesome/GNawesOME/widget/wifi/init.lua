61019e6c299af465f07ba2cf2a7c259baf2ee27d
Gerome Matilla <gerome.matilla07@gmail.com>
2020-01-03 13:51:04 +0800

    wifi-widget cleanup and update

--- config/awesome/GNawesOME/widget/wifi/init.lua
+++ config/awesome/GNawesOME/widget/wifi/init.lua
@@ -1,47 +1,57 @@
 -------------------------------------------------
 -- WiFi Widget for Awesome Window Manager
--- Shows the wifi status using the ACPI tool
+-- Shows the wifi status using some magic
+-- Make sure to change the wireless interface
+-- See `ifconfig` or `iwconfig`
 -------------------------------------------------
 
 local awful = require('awful')
 local watch = require('awful.widget.watch')
 local wibox = require('wibox')
-local clickable_container = require('widget.material.clickable-container')
 local gears = require('gears')
-local dpi = require('beautiful').xresources.apply_dpi
+local naughty = require('naughty') 
 
+local clickable_container = require('widget.material.clickable-container')
+local dpi = require('beautiful').xresources.apply_dpi
 
 local HOME = os.getenv('HOME')
 local PATH_TO_ICONS = HOME .. '/.config/awesome/widget/wifi/icons/'
+
+
 local interface = 'wlp3s0'
 local connected = false
+local status = nil
+local start_up = true
 local essid = 'N/A'
+local wifi_strength = 0
 
 local widget =
   wibox.widget {
   {
     id = 'icon',
-    image = PATH_TO_ICONS .. 'wifi-off' .. '.svg',
+    image = PATH_TO_ICONS .. 'wifi-strength-off' .. '.svg',
     widget = wibox.widget.imagebox,
     resize = true
   },
   layout = wibox.layout.align.horizontal
 }
 
-local widget_button = clickable_container(wibox.container.margin(widget, dpi(7), dpi(7), dpi(7), dpi(7)))
-widget_button:buttons(
-  gears.table.join(
-    awful.button(
-      {},
-      1,
-      nil,
-      function()
-        awful.spawn('wicd-client -n')
-      end
-    )
-  )
-)
+-- Wicd client
+-- local widget_button = clickable_container(wibox.container.margin(widget, dpi(7), dpi(7), dpi(7), dpi(7)))
+-- widget_button:buttons(
+--   gears.table.join(
+--     awful.button(
+--       {},
+--       1,
+--       nil,
+--       function()
+--         awful.spawn('wicd-client -n')
+--       end
+--     )
+--   )
+-- )
 
+-- Network manager
 local widget_button = clickable_container(wibox.container.margin(widget, dpi(7), dpi(7), dpi(7), dpi(7)))
 widget_button:buttons(
   gears.table.join(
@@ -56,7 +66,7 @@ widget_button:buttons(
   )
 )
 
--- Alternative to naughty.notify - tooltip. You can compare both and choose the preferred one
+-- Tooltip
 awful.tooltip(
   {
     objects = {widget_button},
@@ -64,8 +74,11 @@ awful.tooltip(
     align = 'right',
     timer_function = function()
       if connected then
-        return 'Connected to ' .. essid
+
+        return 'Connected to: ' .. essid .. 
+          '\nWiFi-strength: ' .. tostring(wifi_strength) .. '%'
       else
+
         return 'Wireless network is disconnected'
       end
     end,
@@ -75,7 +88,8 @@ awful.tooltip(
   }
 )
 
-local function grabText()
+-- Get ESSID
+local get_essid = function()
   if connected then
     awful.spawn.easy_async(
       'iw dev ' .. interface .. ' link',
@@ -89,34 +103,79 @@ local function grabText()
   end
 end
 
-watch(
-  "awk 'NR==3 {printf \"%3.0f\" ,($3/70)*100}' /proc/net/wireless",
-  5,
+-- Notify the change in connection status
+local notify_connection = function()
+
+  if status ~= connected then
+    status = connected
+
+    if start_up == false then
+      if connected == true then
+        get_essid()
+        naughty.notify({ 
+          text = "You're now connected to WiFi:\n" .. essid,
+          title = "System Notification",
+          icon = PATH_TO_ICONS .. 'wifi.svg',
+          })
+      else
+        naughty.notify({ 
+          text = "WiFi Disconnected",
+          title = "System Notification",
+          icon = PATH_TO_ICONS .. 'wifi-off.svg',
+          })
+      end
+    end
+  end 
+end
+
+
+-- Get wifi strenth bash script
+local get_wifi_strength = [[
+awk 'NR==3 {printf "%3.0f" ,($3/70)*100}' /proc/net/wireless
+]]
+
+watch(get_wifi_strength, 5,
   function(_, stdout)
     local widgetIconName = 'wifi-strength'
-    local wifi_strength = tonumber(stdout)
+    wifi_strength = tonumber(stdout)
     if (wifi_strength ~= nil) then
       connected = true
-      -- Update popup text
+      -- Create a notification
+      notify_connection()
+      -- Get wifi wifi_strength
       local wifi_strength_rounded = math.floor(wifi_strength / 25 + 0.5)
       widgetIconName = widgetIconName .. '-' .. wifi_strength_rounded
+      -- Update wifi strength icon
       widget.icon:set_image(PATH_TO_ICONS .. widgetIconName .. '.svg')
     else
       connected = false
+      -- Create a notification
+      notify_connection()
+      -- Update wifi strength to off
       widget.icon:set_image(PATH_TO_ICONS .. widgetIconName .. '-off' .. '.svg')
     end
+
+    -- If not connected
     if (connected and (essid == 'N/A' or essid == nil)) then
-      grabText()
+      get_essid()
     end
+
+    -- Using this as condition for notify_connection()
+    -- So we don't have a notification every after startup
+    if start_up then
+      start_up = false
+    end
+
+    -- Cleanup memory
     collectgarbage('collect')
-  end,
-  widget
+  end
 )
 
+-- Tooltip text update
 widget:connect_signal(
   'mouse::enter',
   function()
-    grabText()
+    get_essid()
   end
 )
 
