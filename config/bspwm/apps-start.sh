f6e47dd21e9cfcffb3e47811e4dc9a86903357e6
Gerome Matilla <gerome.matilla@gmail.com>
2020-03-21 15:29:43 +0800

    add bspwm config

--- config/bspwm/apps-start.sh
+++ config/bspwm/apps-start.sh
@@ -2,146 +2,46 @@
 
 
 
-#Keyboard Shortcut
-sxhkd &
+# Keyboard Shortcut
 
+sxhkd -c ~/.config/bspwm/configuration/sxhkd/sxhkdrc &
 
+# Restore cursor theme
 
-#xfce goodies
-run_once xfce4-session &
-
-
-
-
-#dpi
-#xrandr --dpi 125.37
-
-
-
-
-#Restore cursor theme
 xsetroot -cursor_name left_ptr
 
+# Restore wallpaper
 
-#WAL
-#run_once wal -Rn &
+feh --bg-fill ${HOME}/Pictures/Wallpapers/no-mans-sky-8k-ultrawide-i3.jpg 
 
+# Music is layf
 
+mpd &>/dev/null
 
+# Compositor
 
+picom --experimental-backends --dbus --config ~/.config/bspwm/configuration/picom/picom.conf &
 
-#XWINWRAP
-#run_once winwrap &
+# Load Xresources
 
+xrdb ${HOME}/.Xresources
 
+# nm-applet 
 
+nm-applet &>/dev/null
 
+# blueman applet
 
+blueman-applet &>/dev/null
 
-#Restore wallpaper
-nitrogen --restore
-#feh --bg-scale ~/Pictures/Wallpapers/pink-house.jpg
+# Equalizer
 
+pulseeffects --gapplication-service &>/dev/null
 
+# Polkit
 
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/null
 
+# Keyring 
 
-
-#Music is layf
-run_once mpd &
-
-
-
-
-
-
-
-
-#Notification
-pkill dunst
-run_once dunst &
-
-
-
- 
-
-
-
-
-#Compositor
-run_once compton &
-
-
-
-
-
-
-#Enable tap
-#run_once synclient TapButton1=1 
-
-
-
-
-
-#flashfocus
-#pkill flashfocus
-#flashfocus &
-
-
-
-
-
-
-## hides mouse when you are not using it. When mouse is hidden, rootmenu considers mouse on desktop, so actions can be triggered.
-run_once unclutter &
-
-
-
-
-
-#Termial Emulator
-run_once urxvtd &
-
-
-#Tint2
-sh $HOME/.config/tint2/launchtint.sh &
-sleep 3
-
-
-
-#lock in idle
-#xautolock -time 1 -locker "run_once betterlockscreen -l &"
-
-
-
-
-
-#Snowwwww
-#pkill xsnow
-#xsnow -nosanta -notrees -norudolf -xspeed 10 -windtimer 5
-
-
-
-#Auto-hide sys-tray
-#pkill hideIt.sh
-#sleep 5
-#hideIt.sh --name '^Polybar tray window$' --signal --peek 0 --direction right
-
-
-
-
-#Conky
-#sh $HOME/.config/conky/launch.sh  &
-
-
-#Polybar
-sh $HOME/.config/polybar/launch.sh
-#sh $HOME/.config/polybar/launch2.sh
-
-
-
-run_once xdo raise -N Plank
-
-
-
-bottint
+eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &>/dev/null
