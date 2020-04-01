425db591b6bef2e995affbceb8cf99e5273cac16
Gerome Matilla <gerome.matilla@gmail.com>
2020-04-01 17:36:52 +0800

    theme:add tooltip gaps

--- config/awesome/linear/theme/default-theme.lua
+++ config/awesome/linear/theme/default-theme.lua
@@ -164,6 +164,7 @@ local awesome_overrides = function(theme)
 	theme.tooltip_bg = theme.background
 	theme.tooltip_border_color = theme.transparent
 	theme.tooltip_border_width = 0
+	theme.tooltip_gaps = dpi(5)
 	theme.tooltip_shape = function(cr, w, h)
 		gears.shape.rounded_rect(cr, w, h, dpi(6))
 	end
