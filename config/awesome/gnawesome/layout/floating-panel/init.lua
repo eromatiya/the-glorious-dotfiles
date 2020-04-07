76c77c866715dbe3d450bf22ad757dd3a4d1eeca
Gerome Matilla <gerome.matilla07@gmail.com>
2020-04-07 08:08:20 +0800

    awesome:gnawesome add tooltip on click widget

--- config/awesome/gnawesome/layout/floating-panel/init.lua
+++ config/awesome/gnawesome/layout/floating-panel/init.lua
@@ -58,6 +58,60 @@ local return_button = function()
 		)
 	)
 
+
+	local clock_tooltip = awful.tooltip
+	{
+		objects = {widget_button},
+		mode = 'outside',
+		delay_show = 1,
+		preferred_positions = {'right', 'left', 'top', 'bottom'},
+		preferred_alignments = {'middle'},
+		margin_leftright = dpi(8),
+		margin_topbottom = dpi(8),
+		timer_function = function()
+			local ordinal = nil
+
+			local day = os.date('%d')
+			local month = os.date('%B')
+
+			local first_digit = string.sub(day, 0, 1) 
+			local last_digit = string.sub(day, -1) 
+
+			if first_digit == '0' then
+			  day = last_digit
+			end
+
+
+			if last_digit == '1' and day ~= '11' then
+			  ordinal = 'st'
+			elseif last_digit == '2' and day ~= '12' then
+			  ordinal = 'nd'
+			elseif last_digit == '3' and day ~= '13' then
+			  ordinal = 'rd'
+			else
+			  ordinal = 'th'
+			end
+
+			local date_str = 'Today is the ' ..
+			'<b>' .. day .. ordinal .. 
+			' of ' .. month .. '</b>.\n' ..
+			'And it\'s fucking ' .. os.date('%A')
+
+			return date_str
+
+		end,
+	}
+	
+	widget_button:connect_signal(
+		'button::press', 
+		function(self, lx, ly, button)
+			-- Hide the tooltip when you press the clock widget
+			if clock_tooltip.visible and button == 1 then
+				clock_tooltip.visible = false
+			end
+		end
+	)
+
 	return widget_button
 
 end
