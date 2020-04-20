d5ed49903d2b26ab54fcf06c076417d162ec4138
Gerome Matilla <gerome.matilla07@gmail.com>
2020-04-21 07:16:46 +0800

    remove old unused binaries

--- binaries/old binaries/notif-battery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-status="$(< /sys/class/power_supply/BAT0/status)"
-capacity="$(< /sys/class/power_supply/BAT0/capacity)"
-
-dunstify -r 100 "Your Lovely Battery! :)" "Battery percentage is $capacity% and is currenly $status."
