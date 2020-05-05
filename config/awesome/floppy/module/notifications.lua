88f2f906441e960760a280db348bde3c7a0d8f26
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-05 15:31:29 +0800

    fix notification timeout

--- config/awesome/floppy/module/notifications.lua
+++ config/awesome/floppy/module/notifications.lua
@@ -62,7 +62,6 @@ ruled.notification.connect_signal('request::rules', function()
 			fg 					= beautiful.fg_normal,
 			margin 				= dpi(16),
 			position 			= 'top_right',
-			timeout 			= 5,
 			implicit_timeout 	= 5
 		}
 	}
@@ -76,6 +75,7 @@ ruled.notification.connect_signal('request::rules', function()
 			fg 					= beautiful.fg_normal,
 			margin 				= dpi(16),
 			position 			= 'top_right',
+			timeout 			= 5,
 			implicit_timeout	= 5
 		}
 	}
