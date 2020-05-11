c6a77c0256c3c0760668f6e94371725f216de450
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-11 16:07:31 +0800

    config:apps.lua cleanup, replace global lockscreen function with signal

--- config/awesome/gnawesome/configuration/apps.lua
+++ config/awesome/gnawesome/configuration/apps.lua
@@ -4,31 +4,33 @@ local config_dir = filesystem.get_configuration_dir()
 local bin_dir = config_dir .. 'binaries/'
 
 
+awesome.connect_signal('test', function() require('awful').screen.focused().top_panel.bg = '#ff00ff' end)
+
 return {
 
 	-- The default applications in keybindings and widgets
 	default = {
-		terminal 												= 'kitty',                                  							-- Terminal Emulator
-		text_editor 											= 'subl3',                                  						-- GUI Text Editor
-		web_browser 										= 'firefox',                                						-- Web browser
-		file_manager 										= 'dolphin',                              	 	 					-- GUI File manager
-		network_manager 								= 'nm-connection-editor',								-- Network manager
-		bluetooth_manager 								= 'blueman-manager',										-- Bluetooth manager
-		power_manager 									= 'xfce4-power-manager',								-- Power manager
-		package_manager 								= 'pamac-manager',											-- GUI Package manager
-		lock 														= 'awesome-client "_G.show_lockscreen()"',  	-- Lockscreen
-		quake 													= 'kitty --name QuakeTerminal',       			    -- Quake-like Terminal
+		terminal 										= 'kitty',																-- Terminal Emulator
+		text_editor 									= 'subl3',                      	            						-- GUI Text Editor
+		web_browser 									= 'firefox',                        	        						-- Web browser
+		file_manager 									= 'dolphin',                            	  	 	 					-- GUI File manager
+		network_manager 								= 'nm-connection-editor',												-- Network manager
+		bluetooth_manager 								= 'blueman-manager',													-- Bluetooth manager
+		power_manager 									= 'xfce4-power-manager',												-- Power manager
+		package_manager 								= 'pamac-manager',														-- GUI Package manager
+		lock 											= 'awesome-client "awesome.emit_signal(\'module::lockscreen_show\')"',	-- Lockscreen
+		quake 											= 'kitty --name QuakeTerminal',       			    					-- Quake-like Terminal
 
-		rofiglobal												= 'rofi -dpi ' .. screen.primary.dpi .. 
-																	    ' -show "Global Search" -modi "Global Search":' .. config_dir .. 
-																	    '/configuration/rofi/sidebar/rofi-spotlight.sh' .. 
-																	    ' -theme ' .. config_dir ..
-																	    '/configuration/rofi/sidebar/rofi.rasi', 			-- Rofi Web Search
+		rofiglobal										= 'rofi -dpi ' .. screen.primary.dpi .. 
+															' -show "Global Search" -modi "Global Search":' .. config_dir .. 
+															'/configuration/rofi/sidebar/rofi-spotlight.sh' .. 
+															' -theme ' .. config_dir ..
+															'/configuration/rofi/sidebar/rofi.rasi',			 				-- Rofi Web Search
 		
 
-		rofiappmenu 										= 'rofi -dpi ' .. screen.primary.dpi ..
-																	    ' -show drun -theme ' .. config_dir ..
-																	    '/configuration/rofi/appmenu/rofi.rasi'  		-- Application Menu
+		rofiappmenu 									= 'rofi -dpi ' .. screen.primary.dpi ..
+															' -show drun -theme ' .. config_dir ..
+															'/configuration/rofi/appmenu/rofi.rasi'					  			-- Application Menu
 
 		-- You can add more default applications here
 	},
@@ -38,20 +40,21 @@ return {
 	run_on_start_up = {
 
 		'picom -b --experimental-backends --dbus --config ' .. 
-		config_dir .. '/configuration/picom.conf',   																	-- Compositor
+		config_dir .. '/configuration/picom.conf',   																			-- Compositor
 
-		'blueman-applet',                                                 								                        -- Bluetooth tray icon
-		'mpd',                                                                    										        	    -- Music Server
-		'xfce4-power-manager',                                                                  					    	-- Power manager
+		'blueman-applet',                                           	      								                    -- Bluetooth tray icon
+		'mpd',                                                          	          										    -- Music Server
+		'xfce4-power-manager',                                              	                    					    	-- Power manager
 		'/usr/lib/polkit-kde-authentication-agent-1 &' .. 
-		' eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)',           -- Credential manager
+		' eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', 	          									-- Credential manager
 		
-		'xrdb $HOME/.Xresources',                                                                   						-- Load X Colors
-		'nm-applet',                                                                                									-- NetworkManager Applet
-		'pulseeffects --gapplication-service',                                                      						-- Sound Equalizer
-		
-		'xidlehook --not-when-fullscreen --not-when-audio --timer 600 '..
-		' "awesome-client \'_G.show_lockscreen()\'" ""'  															-- Auto lock timer
+		'xrdb $HOME/.Xresources',                                                   	                						-- Load X Colors
+		'nm-applet',                                                                    	            						-- NetworkManager Applet
+		'pulseeffects --gapplication-service',                                              	        						-- Sound Equalizer
+		[[
+		xidlehook --not-when-fullscreen --not-when-audio --timer 600 \
+		"awesome-client 'awesome.emit_signal(\"module::lockscreen_show\")'" ""
+		]]																														-- Auto lock timer 
 
 		-- You can add more start-up applications here
 	},
@@ -59,8 +62,8 @@ return {
 	-- List of binaries that will execute a certain task
 
 	bins = {
-		full_screenshot = bin_dir .. 'snap full',              					                    					-- Full Screenshot
-		area_screenshot = bin_dir .. 'snap area',			                                        					-- Area Selected Screenshot
-		update_profile  = bin_dir .. 'profile-image'
+		full_screenshot = bin_dir .. 'snap full',              					                    							-- Full Screenshot
+		area_screenshot = bin_dir .. 'snap area',			                                        							-- Area Selected Screenshot
+		update_profile  = bin_dir .. 'profile-image'																			-- Update profile picture
 	}
 }
