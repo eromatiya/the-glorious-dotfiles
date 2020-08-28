8c2ddd2d846b98a2bf3e6b4b4501e37d09ed6f31
Gerome Matilla <gerome.matilla07@gmail.com>
2020-08-28 15:58:05 +0800

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,32 @@
-# Remove greeting
-set fish_greeting
+# Global colors - mostly used by fish_prompt.fish
+set -g foreground		'#F8F8F2'
+set -g black					'#3D4C5F'
+set -g red						'#EE4F84'
+set -g green					'#53E2AE'
+set -g yellow				'#F1FF52'
+set -g blue					'#6498EF'
+set -g magenta			'#985EFF'
+set -g cyan					'#24D1E7'
+set -g white					'#E5E5E5'
+
+# Some config
+set -g fish_greeting
 set -g theme_nerd_fonts yes
 set -g theme display_user yes
-set -g default_user dumbass
 
-# Remove pastfish greeting
-function fish_greeting
-end
+# Git config
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_color_branch $magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_showdirtystate "yes"
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_color_stagedstate $yellow
+set -g __fish_git_prompt_color_invalidstate $red
+set -g __fish_git_prompt_char_cleanstate '✔'
+set -g __fish_git_prompt_color_cleanstate $green
+set -g __fish_git_prompt_color_branch $white
+set -g __fish_git_prompt_color_prefix $white
+set -g __fish_git_prompt_color_suffix $white
 
 # Get terminal emulator
 set TERM_EMULATOR (basename "/"(ps -f -p (cat /proc/(echo %self)/stat | cut -d \  -f 4) | tail -1 | sed 's/^.* //'))
@@ -44,3 +64,14 @@ end
 # User aliases
 alias ytmp3='youtube-dl --extract-audio --audio-format mp3'
 alias cls='clear'
+
+export GREP_OPTIONS="--color=auto"; # make grep colorful
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD; # make ls more colorful as well
+export HISTSIZE=32768; # Larger bash history (allow 32³ entries; default is 500)
+export HISTFILESIZE=$HISTSIZE;
+export HISTCONTROL=ignoredups; # Remove duplicates from history. I use `git status` a lot.
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"; # Make some commands not show up in history
+export LANG="en_US.UTF-8"; # Language formatting is still important
+export LC_ALL="en_US.UTF-8"; # byte-wise sorting and force language for those pesky apps
+export MANPAGER="less -X"; # Less is more
+export GPG_TTY=(tty); # for gpg key management
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,88 +1,119 @@
-
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+function git_dirty
+	echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
-function _inteval
-  math "abs($argv[1] - $argv[2])"
+function background_normal
+	# Prevents background color to "overflow"
+	echo (set_color -b normal)
+end
+
+function distro_icon
+	# TODO: Automatically detect distro and set its logo
+	#  Tux
+	#  Archlinux
+	#  Debian
+	#  Ubuntu
+	#  Manjaro
+	#  Centos
+	#  Fedora
+	#  Mint
+	#  Alpine
+	#  Devuan
+	#  Apple
+	#  Docker
+	#  Opensuse
+	#  Slackware
+	#  Redhat
+	#  Elementary
+	#  Fire
+	# Sauce: https://nerdfonts.com/cheat-sheet
+
+	set icon '  '
+	set prompt_distro (set_color -b $blue $white)$icon
+	echo $prompt_distro
+end
+
+function interval
+	math "abs($argv[1] - $argv[2])"
+end
+
+function get_time
+	set sec (math "round("(date +%H)"*12 + "(date +%M)"/5)")
+	set r (echo "obase=16; "(math (interval $sec 144)"+ 111") | bc)
+	set g (echo "obase=16; "(math (interval (math "($sec + 96) % 288") 144 )"+ 111") | bc)
+	set b (echo "obase=16; "(math (interval (math "($sec + 192) % 288") 144 )"+ 111") | bc)
+	set bg $r$g$b
+	set prompt_time (set_color -b $bg -o $black)' '(date +%H:%M)' '
+	echo $prompt_time
+end
+
+function user_host
+	# User is root.
+	if [ $USER = 'root' ]
+		set -g user_bg $red
+	else if [ $USER != (logname) ]
+		# User is not login user.
+		set -g user_bg $yellow
+	else
+		set -g user_bg $white
+	end
+	set prompt_user (set_color -b $user_bg -o black)' '(whoami)@(hostname)' '
+
+	# Connected on remote machine, via ssh (good).
+	if [ $SSH_CONNECTION ]
+		set prompt_user  $prompt_user(set_color -b $cyan -o black)' '(hostname)' '
+	end
+	echo $prompt_user
+end
+
+function git_prompt
+	set prompt_git (set_color -b normal)(fish_git_prompt)
+	echo $prompt_git
+end
+
+function pwd_prompt
+	set prompt_pwd (set_color -b $black -o $blue)' '(prompt_pwd)' '
+	echo $prompt_pwd
+end
+
+function last_status
+	if not test $prev_status -eq 0
+		set_color $fish_color_error
+		echo -n (set_color -b $red $yellow) '✖ '
+	else
+		echo -n (set_color -b $cyan $black) '✔ '
+	end
+	set_color normal
 end
 
 function fish_prompt
+	set -g prev_status $status
+	# # Check working directory
+	# if test -w $PWD
+	# 	# if has write permission
+	# 	if [ (git_dirty) ]
+	# 		# if git modified
+	# 		set arrow_color $yellow
+	# 	else
+	# 		set arrow_color $white
+	# 	end
+	# else
+	# 	set arrow_color $red
+	# end
 
-  # if has cache
-  if not [ $staticPrompt ]
-    # Test user type:
-    if [ $USER = "root" ]
-        # User is root.
-        set color "red"
-    else if [ $USER != (logname) ]
-        # User is not login user.
-        set color "yellow"
-    else
-        # User is normal (well ... most of us are).
-        set color "white"
-    end
+	# Window title
+	switch $TERM;
+		case xterm'*' vte'*';
+			printf '\033]0;['(prompt_pwd)']\007';
+		case screen'*';
+ 			printf '\033k['(prompt_pwd)']\033\\';
+ 	end
 
-    set promptTmp (set_color -b $color -o black)' '$USER' '
+ 	# Print prompt
+	printf '%s%s%s%s ' (distro_icon) (user_host) (pwd_prompt) (background_normal) (__fish_git_prompt) (background_normal)
+end
 
-    # if is SSH type:
-    if [ $SSH_CONNECTION ]
-        # Connected on remote machine, via ssh (good).
-        set color "blue"
-        set promptTmp  $promptTmp(set_color -b $color -o black)' '(hostname)' '
-    end
-
-    # save result to cache
-    set -g staticPrompt $promptTmp
-    set -g staticColor $color
-  else # grab cache
-    set promptTmp $staticPrompt
-    set color $staticColor
-  end
-
-  # determine color of time
-  switch $TERM
-    case '*'256'*' # support 256color
-      set minToday (math "round("(date +%H)"*12 + "(date +%M)"/5)")
-      set color_R (echo "obase=16; "(math (_inteval $minToday 144)"+ 111") | bc)
-      set color_G (echo "obase=16; "(math (_inteval (math "($minToday + 96) % 288") 144 )"+ 111") | bc)
-      set color_B (echo "obase=16; "(math (_inteval (math "($minToday + 192) % 288") 144 )"+ 111") | bc)
-      set color $color_R$color_G$color_B
-    case '*' # not support 256color
-      set color "#92B6F4"
-  end
-
-  # for debugging
-  # echo $color
-  # set color "cyan"
-
-  # time with color
-  set promptTmp $promptTmp(set_color -b $color -o black)' '(date +%H:%M)' '
-
-  # test current working directory
-  if test -w $PWD # if has write permission
-    if [ (_is_git_dirty) ] # if git modified
-      set posColor yellow
-    else
-      set posColor white
-    end
-  else
-    set posColor red
-  end
-
-  # PWD
-  set promptTmp $promptTmp(set_color -b black -o $color)' '(prompt_pwd)' '(set_color -b normal $posColor)' > '
-
-  # ========================================
-  # tmux, screen title
-  switch $TERM;
-    case xterm'*' vte'*';
-      printf '\033]0;['(prompt_pwd)']\007';
-    case screen'*';
-      printf '\033k['(prompt_pwd)']\033\\';
-  end
-  # ========================================
-
-  # output
-  echo $promptTmp
+function fish_right_prompt
+	# Print right prompt
+	printf '%s%s' (background_normal)(last_status) (get_time)(background_normal)
 end
