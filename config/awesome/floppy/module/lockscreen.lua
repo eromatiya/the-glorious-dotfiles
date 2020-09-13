958d187a261ca5fbb7762aa7c039ddc7f6d2b5b3
Gerome Matilla <gerome.matilla07@gmail.com>
2020-09-14 06:19:00 +0800

    module: lockscreen

--- config/awesome/floppy/module/lockscreen.lua
+++ config/awesome/floppy/module/lockscreen.lua
@@ -26,14 +26,14 @@ local locker_config = {
 
 	-- General Configuration
 	-- Capture a picture using webcam
-	capture_intruder = config.module.lockscreen.capture_intruder or true,
+	capture_intruder = config.module.lockscreen.capture_intruder or false,
 
 	-- Save location, auto creates
 	face_capture_dir = config.module.lockscreen.face_capture_dir or '$(xdg-user-dir PICTURES)/Intruders/',
 
 	-- Background Mode Configuration
 	-- True to blur the background
-	blur_background = config.module.lockscreen.blur_background or true,
+	blur_background = config.module.lockscreen.blur_background or false,
 	
 	-- Wallpaper directory
 	wall_dir = config_dir .. (config.module.lockscreen.wall_dir or 'theme/wallpapers/'),
