11c77c7d1a44b488b9d70b10f0c2c6ecd2366aa1
frostedpenguin <baltinon2@gmail.com>
2022-07-21 18:36:35 +0300

    'finalized keyboard widget'

--- config/awesome/floppy/widget/keyboard-layout/init.lua
+++ config/awesome/floppy/widget/keyboard-layout/init.lua
@@ -3,8 +3,6 @@ local keyboardlayout = require("awful.widget.keyboardlayout")
 local wibox = require("wibox")
 local awful = require("awful")
 local gears = require("gears")
--- local recorder_table = require('widget.keyboard-layout.ui')
-require("widget.screen-recorder.screen-recorder-ui-backend")
 
 Layouts_dto = {}
 Curr_layout_index = 1
@@ -13,7 +11,7 @@ local w_keyboard_layout = wibox.widget({
 	align = "center",
 	valign = "center",
 	font = "SF Pro Text Regular 10",
-	text = "us",
+	text = "",
 })
 local initialize_layouts = function()
 	awful.spawn.easy_async_with_shell("awesome-client 'return awesome.xkb_get_group_names()'", function(stdout)
@@ -22,35 +20,29 @@ local initialize_layouts = function()
 			return
 		end
 		local layout_table = keyboardlayout.get_groups_from_group_names(stdout)
-		-- local table = gears.debug.dump_return(layout_table)
-		-- gears.debug.print_warning(table)
 		for i, layout in ipairs(layout_table) do
 			Layouts_dto[i] = layout["file"]
 		end
+	w_keyboard_layout:set_text(Layouts_dto[Curr_layout_index])
 	end)
 	return Layouts_dto
 end
 initialize_layouts()
 
--- gears.debug.print_warning(Layouts_dto[2])
 
 local return_widget = function()
-	-- local group_names = awful.awesome.xkb_get_group_names()
-	-- local group_name = group_names[awesome.xkb_get_group_state()]
 	local update_layout = function()
 		if Curr_layout_index == #Layouts_dto then
 			Curr_layout_index = 0
 		end
 		Curr_layout_index = Curr_layout_index + 1
 		w_keyboard_layout:set_text(Layouts_dto[Curr_layout_index])
-		gears.debug.print_warning(Layouts_dto[1])
+
 	end
 
-	-- local group_names = awesome.xkb_get_group_names()
 	capi.awesome.connect_signal("xkb::group_changed", function()
 		update_layout()
 	end)
-	-- w_keyboard_layout:set_text(group_names)
 	return w_keyboard_layout
 end
 
