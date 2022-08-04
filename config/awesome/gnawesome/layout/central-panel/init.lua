040cd555c16d9355c8a654ba8998307c57639df9
frostedpenguin <baltinon2@gmail.com>
2022-08-04 23:02:13 +0300

    regular commit

--- config/awesome/gnawesome/layout/central-panel/init.lua
+++ config/awesome/gnawesome/layout/central-panel/init.lua
@@ -1,44 +1,50 @@
-local awful = require('awful')
-local wibox = require('wibox')
-local gears = require('gears')
-local beautiful = require('beautiful')
+local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
+local central_panel_switch = require("widget.central-panel-switch")
+local user_profile = require("widget.user-profile")
+local weather = require("widget.weather")
+local email = require("widget.email")
+local calendar = require("widget.calendar")
+local notification_center = require("widget.notif-center")
+local central_panel_settings = require("layout.central-panel.settings")()
 
 local panel_visible = false
 
 local central_panel = function(s)
-
 	-- Set right panel geometry
 	local panel_width = dpi(700)
 	local panel_height = s.geometry.height - dpi(38)
 	local panel_margins = dpi(5)
 
-	local separator = wibox.widget {
-		orientation = 'horizontal',
+	local separator = wibox.widget({
+		orientation = "horizontal",
 		opacity = 0.0,
 		forced_height = 15,
 		widget = wibox.widget.separator,
-	}
+	})
 
-	local panel = awful.popup {
+	local panel = awful.popup({
 		widget = {
 			{
 				{
-					expand = 'none',
+					expand = "none",
 					layout = wibox.layout.fixed.vertical,
 					{
 						layout = wibox.layout.align.horizontal,
-						expand = 'none',
+						expand = "none",
 						nil,
-						require('widget.central-panel-switch'),
-						nil
+						central_panel_switch,
+						nil,
 					},
 					separator,
 					{
 						layout = wibox.layout.stack,
 						-- Today Pane
 						{
-							id = 'pane_id',
+							id = "pane_id",
 							visible = true,
 							layout = wibox.layout.fixed.vertical,
 							{
@@ -47,35 +53,33 @@ local central_panel = function(s)
 								{
 									layout = wibox.layout.fixed.vertical,
 									spacing = dpi(7),
-									require('widget.user-profile'),
-									require('widget.weather'),
-									require('widget.email'),
-									require('widget.calendar')
+									user_profile,
+									weather,
+									email,
+									calendar,
 								},
-								require('widget.notif-center')(s)
+								notification_center(s),
 							},
-
 						},
 						{
-							id = 'settings_id',
+							id = "settings_id",
 							visible = false,
 							layout = wibox.layout.fixed.vertical,
-							require('layout.central-panel.settings')()
-						}
-
+							central_panel_settings(),
+						},
 					},
 				},
 				margins = dpi(16),
-				widget = wibox.container.margin
+				widget = wibox.container.margin,
 			},
 			bg = beautiful.background,
-			shape =function(cr, w, h)
+			shape = function(cr, w, h)
 				gears.shape.rounded_rect(cr, w, h, beautiful.groups_radius)
 			end,
-			widget = wibox.container.background
+			widget = wibox.container.background,
 		},
 		screen = s,
-		type = 'dock',
+		type = "dock",
 		visible = false,
 		ontop = true,
 		width = panel_width,
@@ -84,38 +88,34 @@ local central_panel = function(s)
 		height = s.geometry.height,
 		bg = beautiful.transparent,
 		fg = beautiful.fg_normal,
-		shape = gears.shape.rectangle
-	}
+		shape = gears.shape.rectangle,
+	})
 
-	awful.placement.centered(
-		panel, 
-		{
-			margins = {
-				-- right = panel_margins,
-				top = s.geometry.y + dpi(65)
-			},
-			parent = s
-		}
-	)
+	awful.placement.centered(panel, {
+		margins = {
+			-- right = panel_margins,
+			top = s.geometry.y + dpi(65),
+		},
+		parent = s,
+	})
 
 	panel.opened = false
 
-	s.backdrop_central = wibox
-	{
+	s.backdrop_central = wibox({
 		ontop = true,
 		screen = s,
 		bg = beautiful.transparent,
-		type = 'utility',
+		type = "utility",
 		x = s.geometry.x,
 		y = s.geometry.y,
 		width = s.geometry.width,
-		height = s.geometry.height
-	}
+		height = s.geometry.height,
+	})
 
-	panel:struts {
-		top = 0
-	}
-	
+	panel:struts({
+		top = 0,
+	})
+
 	open_panel = function()
 		local focused = awful.screen.focused()
 		panel_visible = true
@@ -123,7 +123,7 @@ local central_panel = function(s)
 		focused.backdrop_central.visible = true
 		focused.central_panel.visible = true
 
-		panel:emit_signal('opened')
+		panel:emit_signal("opened")
 	end
 
 	close_panel = function()
@@ -132,8 +132,8 @@ local central_panel = function(s)
 
 		focused.central_panel.visible = false
 		focused.backdrop_central.visible = false
-		
-		panel:emit_signal('closed')
+
+		panel:emit_signal("closed")
 	end
 
 	-- Hide this panel when app dashboard is called.
@@ -151,29 +151,20 @@ local central_panel = function(s)
 	end
 
 	function panel:switch_pane(mode)
-		if mode == 'today_mode' then
+		if mode == "today_mode" then
 			-- Update Content
-			panel.widget:get_children_by_id('settings_id')[1].visible = false
-			panel.widget:get_children_by_id('pane_id')[1].visible = true
-		elseif mode == 'settings_mode' then
+			panel.widget:get_children_by_id("settings_id")[1].visible = false
+			panel.widget:get_children_by_id("pane_id")[1].visible = true
+		elseif mode == "settings_mode" then
 			-- Update Content
-			panel.widget:get_children_by_id('pane_id')[1].visible = false
-			panel.widget:get_children_by_id('settings_id')[1].visible = true
+			panel.widget:get_children_by_id("pane_id")[1].visible = false
+			panel.widget:get_children_by_id("settings_id")[1].visible = true
 		end
 	end
 
-	s.backdrop_central:buttons(
-		awful.util.table.join(
-			awful.button(
-				{},
-				1,
-				nil,
-				function()
-					panel:toggle()
-				end
-			)
-		)
-	)
+	s.backdrop_central:buttons(awful.util.table.join(awful.button({}, 1, nil, function()
+		panel:toggle()
+	end)))
 	return panel
 end
 
--- config/awesome/gnawesome/layout/central-panel/settings/init.lua
+++ config/awesome/gnawesome/layout/central-panel/settings/init.lua
@@ -1,18 +1,18 @@
-local awful = require('awful')
-local wibox = require('wibox')
-local gears = require('gears')
-local beautiful = require('beautiful')
+local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
-local clickable_container = require('widget.clickable-container')
-local icons = require('theme.icons')
+local clickable_container = require("widget.clickable-container")
+local icons = require("theme.icons")
 
+local quick_settings = require("layout.central-panel.settings.quick-settings")
+local hardware_monitor = require("layout.central-panel.settings.hardware-monitor")
 return function()
-
-	return wibox.widget {
+	return wibox.widget({
 		layout = wibox.layout.flex.horizontal,
 		spacing = dpi(7),
-		require('layout.central-panel.settings.quick-settings'),
-		require('layout.central-panel.settings.hardware-monitor')
-	}
+		quick_settings,
+		hardware_monitor,
+	})
 end
- 
