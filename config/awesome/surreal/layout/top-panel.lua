da6a07e4bc07e34dcc324a1ad457565076f156e2
frostedpenguin <baltinon2@gmail.com>
2022-08-04 21:52:26 +0300

    fixed surreal

--- config/awesome/surreal/layout/top-panel.lua
+++ config/awesome/surreal/layout/top-panel.lua
@@ -1,72 +1,77 @@
-local wibox = require('wibox')
-local awful = require('awful')
-local gears = require('gears')
-local beautiful = require('beautiful')
-local icons = require('theme.icons')
+local wibox = require("wibox")
+local awful = require("awful")
+local gears = require("gears")
+local beautiful = require("beautiful")
+local icons = require("theme.icons")
 local dpi = beautiful.xresources.apply_dpi
-local clickable_container = require('widget.clickable-container')
-local task_list = require('widget.task-list')
+local clickable_container = require("widget.clickable-container")
+local task_list = require("widget.task-list")
+local tray_toggler = require("widget.tray-toggle")
+local updater = require("widget.package-updater")
+local screen_rec = require("widget.screen-recorder")
+local keyboard_layout = require("widget.keyboard-layout")
+local bluetooth = require("widget.bluetooth")
+local battery = require("widget.battery")
+local network = require("widget.network")
+local control_center_toggle = require("widget.control-center-toggle")
+local global_search = require("widget.global-search")
+local info_center_toggle = require("widget.info-center-toggle")
 
 local top_panel = function(s)
-
-	local panel = wibox
-	{
+	local panel = wibox({
 		ontop = true,
 		screen = s,
-		type = 'dock',
+		type = "dock",
 		height = dpi(28),
 		width = s.geometry.width,
 		x = s.geometry.x,
 		y = s.geometry.y,
 		stretch = false,
 		bg = beautiful.background,
-		fg = beautiful.fg_normal
-	}
+		fg = beautiful.fg_normal,
+	})
 
-	panel:struts
-	{
-		top = dpi(28)
-	}
+	panel:struts({
+		top = dpi(28),
+	})
 
-	panel:connect_signal(
-		'mouse::enter',
-		function() 
-			local w = mouse.current_wibox
-			if w then
-				w.cursor = 'left_ptr'
-			end
+	panel:connect_signal("mouse::enter", function()
+		local w = mouse.current_wibox
+		if w then
+			w.cursor = "left_ptr"
 		end
-	)
-	
-	s.systray = wibox.widget {
+	end)
+
+	s.systray = wibox.widget({
 		visible = false,
 		base_size = dpi(20),
 		horizontal = true,
-		screen = 'primary',
-		widget = wibox.widget.systray
-	}
+		screen = "primary",
+		widget = wibox.widget.systray,
+	})
 
-	local clock 			= require('widget.clock')(s)
-	local layout_box 		= require('widget.layoutbox')(s)
-	local add_button 		= require('widget.open-default-app')(s)
-	s.tray_toggler  		= require('widget.tray-toggle')
-	s.updater 				= require('widget.package-updater')()
-	s.screen_rec 			= require('widget.screen-recorder')()
-	s.bluetooth   			= require('widget.bluetooth')()
-	s.battery     			= require('widget.battery')()
-	s.network       		= require('widget.network')()
-	s.control_center_toggle = require('widget.control-center-toggle')()
-	s.global_search			= require('widget.global-search')()
-	s.info_center_toggle 	= require('widget.info-center-toggle')()
+	local clock = require("widget.clock")(s)
+	local layout_box = require("widget.layoutbox")(s)
+	local add_button = require("widget.open-default-app")(s)
+	s.tray_toggler = tray_toggler
+	s.updater = updater()
+	s.screen_rec = screen_rec()
+	s.bluetooth = bluetooth()
+	s.keyboard_layout = keyboard_layout()
+	s.battery = battery()
+	s.network = network()
+	s.control_center_toggle = control_center_toggle()
+	s.global_search = global_search()
+	s.info_center_toggle = info_center_toggle()
 
-	panel : setup {
+	panel:setup({
 		layout = wibox.layout.align.horizontal,
-		expand = 'none',
+		expand = "none",
 		{
 			layout = wibox.layout.fixed.horizontal,
 			task_list(s),
-			add_button
-		}, 
+			add_button,
+		},
 		clock,
 		{
 			layout = wibox.layout.fixed.horizontal,
@@ -74,7 +79,7 @@ local top_panel = function(s)
 			{
 				s.systray,
 				margins = dpi(5),
-				widget = wibox.container.margin
+				widget = wibox.container.margin,
 			},
 			s.tray_toggler,
 			s.updater,
@@ -84,10 +89,11 @@ local top_panel = function(s)
 			s.battery,
 			s.control_center_toggle,
 			s.global_search,
+			s.keyboard_layout,
 			layout_box,
-			s.info_center_toggle
-		}
-	}
+			s.info_center_toggle,
+		},
+	})
 
 	return panel
 end
