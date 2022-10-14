8784efc93baf8cd9bd31f8523e360c7a48f16406
frostedpenguin <baltinon2@gmail.com>
2022-10-14 16:12:54 +0300

    regular commit

--- config/awesome-new/module/path.lua
+++ config/awesome-new/module/path.lua
@@ -2,7 +2,7 @@ local gtable = require("gears.table")
 
 ---sanitizing path so it doesn.t
 local check_for_delimeters = function(folders)
-	for i, path in ipairs(folders) do
+	for _, path in ipairs(folders) do
 		if path:sub(-1) == "/" then
 			error("Path:" .. path .. " has a trailing slash")
 		end
@@ -13,29 +13,28 @@ end
 --- @class Path
 --- @field path table
 local Path = {}
---- insert path in {"path1", "path2", "path3"} format
----@param o any
----@param path table
+--- insert path in ( "path1", "path2", "path3" ) format
+---@param ... string
 ---@return Path
-function Path:new(o, path)
-	o = o or {}
+function Path:new(...)
+	local o = {}
 	setmetatable(o, self)
 	self.__index = self
-	pcall(check_for_delimeters(path))
-	o.path = path or {}
+	pcall(check_for_delimeters({ ... }))
+	o.path = { ... }
 	return o
 end
 
 function Path:__add(other)
 	return Path:new(nil, gtable.join(self.path, other.path))
 end
---- @operator add: Path
---- @param path Path
-function Path:add(path)
-	self.path = gtable.join(self.path, path)
+--- @param ... string
+function Path:join(...)
+	self.path = gtable.join(self.path, { ... })
+	return self
 end
 function Path:__call()
-	local path = table.concat(self.path, "/"):gsub("//", "/") .. "/"
+	local path = table.concat(self.path, "/") .. "/"
 	return path
 end
 
--- config/awesome-new/theme/directories.lua
+++ config/awesome-new/theme/directories.lua
@@ -9,14 +9,13 @@ local titlebar_themes = {
 	["surreal"] = "spotlight",
 }
 
-local config_dir = Path:new(nil, { filesystem.get_configuration_dir() })
-local theme_root_dir = config_dir + Path:new(nil, { "theme" })
-local curr_theme_dir = theme_root_dir + Path:new(nil, { THEME })
-local wallpaper_dir = curr_theme_dir + Path:new(nil, { "wallpapers" })
-local icons = theme_root_dir + Path:new(nil, { "icons" })
-local titlebar = icons + Path:new(nil, { "titlebar", titlebar_themes[THEME] })
+local theme_root_dir = Path:new(filesystem.get_configuration_dir():gsub("/$", ""), "theme")
+local curr_theme_dir = theme_root_dir:join(THEME)
+local wallpaper_dir = curr_theme_dir:join("wallpapers")
+local icons = theme_root_dir:join("icons")
+local titlebar = icons + Path:new("titlebar", titlebar_themes[THEME])
 
---- @type {["root"]: string,["curr_theme"]: string,["icons"]: string, ["titlebar_icons"]: string, ["wallpapers"]: string}
+--- @type {"root": string,["curr_theme"]: string,["icons"]: string, ["titlebar_icons"]: string, ["wallpapers"]: string}
 local directories = {
 	["root"] = theme_root_dir(),
 	["curr_theme"] = theme_root_dir(),
