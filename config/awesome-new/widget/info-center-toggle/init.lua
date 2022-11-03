39d5b9b7efcf70cc33f8b437ecb66b0010860ac8
frostedpenguin <baltinon2@gmail.com>
2022-11-03 16:59:56 +0200

    fixed today panel on floppy ✨

--- config/awesome-new/widget/info-center-toggle/init.lua
+++ config/awesome-new/widget/info-center-toggle/init.lua
@@ -1,45 +1,37 @@
-local awful = require('awful')
-local wibox = require('wibox')
-local gears = require('gears')
-local dpi = require('beautiful').xresources.apply_dpi
+local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
+local dpi = require("beautiful").xresources.apply_dpi
 local config_dir = gears.filesystem.get_configuration_dir()
-local widget_icon_dir = config_dir .. 'widget/info-center-toggle/icons/'
-local clickable_container = require('widget.clickable-container')
+local widget_icon_dir = config_dir .. "widget/info-center-toggle/icons/"
+local clickable_container = require("widget.clickable-container")
+local settings = require("widget.info-center-toggle.settings")
 
 local return_button = function()
-
-	local widget =
-		wibox.widget {
+	local widget = wibox.widget({
 		{
-			id = 'icon',
-			image = widget_icon_dir .. 'info-center.svg',
+			id = "icon",
+			image = widget_icon_dir .. "info-center.svg",
 			widget = wibox.widget.imagebox,
-			resize = true
+			resize = true,
 		},
-		layout = wibox.layout.align.horizontal
-	}
+		layout = wibox.layout.align.horizontal,
+	})
 
-	local widget_button = wibox.widget {
+	local widget_button = wibox.widget({
 		{
 			widget,
 			margins = dpi(7),
-			widget = wibox.container.margin
+			widget = wibox.container.margin,
 		},
-		widget = clickable_container
-	}
+		widget = clickable_container,
+	})
 
-	widget_button:buttons(
-		gears.table.join(
-			awful.button(
-				{},
-				1,
-				nil,
-				function()
-					awful.screen.focused().info_center:toggle()
-				end
-			)
-		)
-	)
+	widget_button:buttons(gears.table.join(awful.button({}, 1, nil, function()
+		local focused_screen = awful.screen.focused()
+		local theme_panel = settings[THEME]
+		focused_screen[theme_panel]:toggle()
+	end)))
 
 	return widget_button
 end
--- config/awesome-new/widget/temperature-meter/init.lua
+++ config/awesome-new/widget/temperature-meter/init.lua
@@ -1,60 +1,60 @@
-local wibox = require('wibox')
-local awful = require('awful')
-local gears = require('gears')
-local beautiful = require('beautiful')
+local wibox = require("wibox")
+local awful = require("awful")
+local gears = require("gears")
+local beautiful = require("beautiful")
 local watch = awful.widget.watch
 local dpi = beautiful.xresources.apply_dpi
-local icons = require('theme.icons')
+local icons = require("theme." .. THEME .. ".icons")
 
-local meter_name = wibox.widget {
-	text = 'Temperature',
-	font = 'Inter Bold 10',
-	align = 'left',
-	widget = wibox.widget.textbox
-}
+local meter_name = wibox.widget({
+	text = "Temperature",
+	font = "Inter Bold 10",
+	align = "left",
+	widget = wibox.widget.textbox,
+})
 
-local icon = wibox.widget {
+local icon = wibox.widget({
 	layout = wibox.layout.align.vertical,
-	expand = 'none',
+	expand = "none",
 	nil,
 	{
 		image = icons.thermometer,
 		resize = true,
-		widget = wibox.widget.imagebox
+		widget = wibox.widget.imagebox,
 	},
-	nil
-}
+	nil,
+})
 
-local meter_icon = wibox.widget {
+local meter_icon = wibox.widget({
 	{
 		icon,
 		margins = dpi(5),
-		widget = wibox.container.margin
+		widget = wibox.container.margin,
 	},
 	bg = beautiful.groups_bg,
 	shape = function(cr, width, height)
 		gears.shape.rounded_rect(cr, width, height, beautiful.groups_radius)
 	end,
-	widget = wibox.container.background
-}
+	widget = wibox.container.background,
+})
 
-local slider = wibox.widget {
+local slider = wibox.widget({
 	nil,
 	{
-		id 				 = 'temp_status',
-		max_value     	 = 100,
-		value         	 = 29,
-		forced_height 	 = dpi(24),
-		color 			 = '#f2f2f2EE',
-		background_color = '#ffffff20',
-		shape 			 = gears.shape.rounded_rect,
-		widget        	 = wibox.widget.progressbar
+		id = "temp_status",
+		max_value = 100,
+		value = 29,
+		forced_height = dpi(24),
+		color = "#f2f2f2EE",
+		background_color = "#ffffff20",
+		shape = gears.shape.rounded_rect,
+		widget = wibox.widget.progressbar,
 	},
 	nil,
-	expand = 'none',
+	expand = "none",
 	forced_height = dpi(36),
-	layout = wibox.layout.align.vertical
-}
+	layout = wibox.layout.align.vertical,
+})
 
 local max_temp = 80
 
@@ -76,26 +76,22 @@ awful.spawn.easy_async_with_shell(
 	done
 	]],
 	function(stdout)
-		local temp_path = stdout:gsub('%\n', '')
-		if temp_path == '' or not temp_path then
-			temp_path = '/sys/class/thermal/thermal_zone0/temp'
+		local temp_path = stdout:gsub("%\n", "")
+		if temp_path == "" or not temp_path then
+			temp_path = "/sys/class/thermal/thermal_zone0/temp"
 		end
 
-		watch(
-			[[
+		watch([[
 			sh -c "cat ]] .. temp_path .. [["
-			]],
-			10,
-			function(_, stdout)
-				local temp = stdout:match('(%d+)')
-				slider.temp_status:set_value((temp / 1000) / max_temp * 100)
-				collectgarbage('collect')
-			end
-		)
+			]], 10, function(_, stdout)
+			local temp = stdout:match("(%d+)")
+			slider.temp_status:set_value((temp / 1000) / max_temp * 100)
+			collectgarbage("collect")
+		end)
 	end
 )
 
-local temp_meter = wibox.widget {
+local temp_meter = wibox.widget({
 	layout = wibox.layout.fixed.vertical,
 	spacing = dpi(5),
 	meter_name,
@@ -104,18 +100,18 @@ local temp_meter = wibox.widget {
 		spacing = dpi(5),
 		{
 			layout = wibox.layout.align.vertical,
-			expand = 'none',
+			expand = "none",
 			nil,
 			{
 				layout = wibox.layout.fixed.horizontal,
 				forced_height = dpi(24),
 				forced_width = dpi(24),
-				meter_icon
+				meter_icon,
 			},
-			nil
+			nil,
 		},
-		slider
-	}
-}
+		slider,
+	},
+})
 
 return temp_meter
