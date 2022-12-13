687886ee6235854529bab287865eed7a0323c63f
frostedpenguin <afrostedpenguin@gmail.com>
2022-12-13 12:48:46 +0200

    added error handling on watch

--- config/awesome/widget/meters/entities/meter.lua
+++ config/awesome/widget/meters/entities/meter.lua
@@ -2,34 +2,33 @@ local wibox = require("wibox")
 local awful = require("awful")
 local gears = require("gears")
 local beautiful = require("beautiful")
-local watch = awful.widget.watch
 local dpi = beautiful.xresources.apply_dpi
 
 ---@param meter_name unknown
 ---@param meter_icon unknown
 ---@param meter_slider unknown
 local return_button = function(meter_name, meter_icon, meter_slider)
-	return wibox.widget({
-		layout = wibox.layout.fixed.vertical,
-		spacing = dpi(5),
-		meter_name,
-		{
-			layout = wibox.layout.fixed.horizontal,
-			spacing = dpi(5),
-			{
-				layout = wibox.layout.align.vertical,
-				expand = "none",
-				nil,
-				{
-					layout = wibox.layout.fixed.horizontal,
-					forced_height = dpi(24),
-					forced_width = dpi(24),
-					meter_icon,
-				},
-				nil,
-			},
-			meter_slider,
-		},
-	})
+  return wibox.widget({
+    layout = wibox.layout.fixed.vertical,
+    spacing = dpi(5),
+    meter_name,
+    {
+      layout = wibox.layout.fixed.horizontal,
+      spacing = dpi(5),
+      {
+        layout = wibox.layout.align.vertical,
+        expand = "none",
+        nil,
+        {
+          layout = wibox.layout.fixed.horizontal,
+          forced_height = dpi(24),
+          forced_width = dpi(24),
+          meter_icon,
+        },
+        nil,
+      },
+      meter_slider,
+    },
+  })
 end
 return return_button
